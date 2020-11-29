b17a690a7647093a7495c33a6a7afc5c0212ce0d
humphrey-mutuma <humphreymutuma01@gmail.com>
2020-11-29 15:35:09 +0300

    Authentication with Passport

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var passport = require('passport');
 var authenticate = require('./authenticate');
-
+const config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,7 +21,7 @@ const  mongoose = require('mongoose');
 // const Leaders = require('./models/leaders')
 // const promotions = require('./models/promotions')
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 
 const connect = mongoose.connect(url);
 
@@ -40,37 +40,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser('12345-67890-09876-54321'));
 
-app.use(session({
-  name:'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
+// app.use(session({
+//   name:'session-id',
+//   secret: '12345-67890-09876-54321',
+//   saveUninitialized: false,
+//   resave: false,
+//   store: new FileStore()
+// }));
 
 app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.session());
 
 // routes before authentication
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // authorization
-  function auth (req, res, next) {
-    console.log(req.user);
-
-    if (!req.user) {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      next(err);
-    }
-    else {
-          next();
-    }
-  }
-
-
-app.use(auth);
 
 
 app.use(express.static(path.join(__dirname, 'public')));
